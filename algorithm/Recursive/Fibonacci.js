6dbe6f92a7a92b65cb7713a82cbffe33b747ce0c
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2023-05-23 01:14:12 +0600

    Recursive algorithm is completed

---
+++ algorithm/Recursive/Fibonacci.js
@@ -0,0 +1,15 @@
+export function fibonacciIterative(n) {
+  const arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[n];
+}
+
+export function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
--- algorithm/Recursive/index.js
+++ algorithm/Recursive/index.js
@@ -1,7 +1,10 @@
 import { findFactorialIterative, findFactorialRecursive } from "./Factorial";
+import { fibonacciIterative, fibonacciRecursive } from "./Fibonacci";
 
 export function recursiveFunc() {
-  console.log("Recursive algorithm is running!");
+  console.log("Recursive algorithm is completed!");
   //   console.log(findFactorialRecursive(5));
-  console.log(findFactorialIterative(7));
+  //   console.log(findFactorialIterative(7));
+  //   console.log(fibonacciIterative(7));
+  //   console.log(fibonacciRecursive(7));
 }
