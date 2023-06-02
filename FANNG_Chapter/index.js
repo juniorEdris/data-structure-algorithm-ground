f95b19802598a8c0acfd2891598cde75e68ab405
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2023-06-02 09:27:51 +0600

    solved an hard leet code solution

--- FANNG_Chapter/index.js
+++ FANNG_Chapter/index.js
@@ -1,3 +1,43 @@
 export function FAANGFunc() {
   console.log("FAANG Chapter started!");
+  // console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+}
+
+/**
+ * 
+ * https://leetcode.com/problems/trapping-rain-water/
+ * 
+ * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]  Output: 6
+    
+  Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+ */
+
+function trap(height) {
+  let totalWater = 0;
+
+  for (let p = 0; p < height.length; p++) {
+    let leftP = p,
+      rightP = p,
+      maxLeft = 0,
+      maxRight = 0;
+
+    while (leftP >= 0) {
+      maxLeft = Math.max(maxLeft, height[leftP]);
+      leftP--;
+    }
+
+    while (rightP < height.length) {
+      // 12
+      maxRight = Math.max(maxRight, height[rightP]);
+      rightP++;
+    }
+
+    const currentWater = Math.min(maxLeft, maxRight) - height[p];
+
+    if (currentWater >= 0) {
+      console.log({ maxLeft, maxRight });
+      totalWater += currentWater;
+    }
+  }
+  return totalWater;
 }
